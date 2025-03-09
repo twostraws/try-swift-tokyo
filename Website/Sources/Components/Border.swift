44f4f44f9a979a75937e67057f61c6c0c54e7295
Paul Hudson <paul@hackingwithswift.com>
2025-03-09 16:54:29 +0000

    Switched the section header border to be a divider.

--- Website/Sources/Components/Border.swift
+++ Website/Sources/Components/Border.swift
@@ -4,6 +4,7 @@ struct Border: HTML {
   var hex: String
 
   var body: some HTML {
-    Text(markdown: "<hr style=\"border: 1px solid \(hex);\">")
+    Divider()
+      .style(.border, "1px solid \(hex)")
   }
 }
